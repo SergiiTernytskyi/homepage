1cc3c4db76192a873146fa665a575158047d2285
Sergii Ternytskyi <serhij86@gmail.com>
2023-12-10 17:47:27 +0200

    skills info added

---
+++ scripts/createMarkup.js
@@ -0,0 +1,13 @@
+export const skillsListMarkup = () => {
+  return `HTML, CSS3, JavaScript, React, Node.js`;
+};
+
+export const skillsLogoListMarkup = () => {
+  return `<div class="skills__list">
+            <img class="skills__logo" src="./assets/HTML5_Badge_512.png"/>
+            <img class="skills__logo" src="./assets/CSS3_logo.svg.png" />
+            <img class="skills__logo" src="./assets/javascript-logo.png"/>
+            <img class="skills__logo" src="./assets/React-icon.svg.png" />
+            <img class="skills__logo" src="./assets/nodejs-logo.svg" />
+          </div>`;
+};
---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+import "./skillsListRender.js";
